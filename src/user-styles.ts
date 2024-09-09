588d5a8e7575075b6b5af127409267c33af12c91
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-09-08 21:29:27 -0400

    publish

--- src/user-styles.ts
+++ src/user-styles.ts
@@ -1,57 +1,57 @@
 import fixedStyles from "./fixed.css?inline";
 
 function createFixedPosition(fixed: string | null) {
-	const distance = "28px";
-	const fixedPosition = `position: fixed; bottom: ${distance}; z-index: 9999;`;
+  const distance = "28px";
+  const fixedPosition = `position: fixed; bottom: ${distance}; z-index: 9999;`;
 
-	if (!fixed) {
-		return "";
-	}
+  if (!fixed) {
+    return "";
+  }
 
-	const fixLowercase = fixed.toLowerCase();
+  const fixLowercase = fixed.toLowerCase();
 
-	if (fixLowercase === "left" || fixLowercase === "right") {
-		return `${fixedPosition} ${fixLowercase}: ${distance};`;
-	}
+  if (fixLowercase === "left" || fixLowercase === "right") {
+    return `${fixedPosition} ${fixLowercase}: ${distance};`;
+  }
 
-	if (fixLowercase === "center") {
-		return `${fixedPosition} left: 50%; translate: -50% 0;`;
-	}
+  if (fixLowercase === "center") {
+    return `${fixedPosition} left: 50%; translate: -50% 0;`;
+  }
 
-	console.error(
-		`[Share Button] It looks you specified an unknown value (${fixed}) for the fixed attribute. Available options are "left," "right," and "center"`,
-	);
-	return "";
+  console.error(
+    `[Share Button] It looks you specified an unknown value (${fixed}) for the fixed attribute. Available options are "left," "right," and "center"`
+  );
+  return "";
 }
 
 function createCustomStyle(el: HTMLElement, variables: string[]) {
-	return variables
-		.map((variable) => {
-			const value = el.getAttribute(variable);
-			if (!value) {
-				return "";
-			}
+  return variables
+    .map((variable) => {
+      const value = el.getAttribute(variable);
+      if (!value) {
+        return "";
+      }
 
-			return `--${variable}: ${value};`;
-		})
-		.join("\n");
+      return `--${variable}: ${value};`;
+    })
+    .join("\n");
 }
 
-
 const userStyles = [
-	"color-button-text",
-	"color-button-bg",
-	"color-popover-bg",
-	"color-popover-text",
+  "color-button-text",
+  "color-button-bg",
+  "color-popover-bg",
+  "color-popover-bg-accent",
+  "color-popover-text",
 ];
 
 export function createUserStyles(el: HTMLElement) {
-	const position = el.getAttribute("position");
-	const fixedPositionStyles = createFixedPosition(position);
-	const customStyles = createCustomStyle(el, userStyles);
-	const baseFixedStyles = position ? fixedStyles : "";
+  const position = el.getAttribute("position");
+  const fixedPositionStyles = createFixedPosition(position);
+  const customStyles = createCustomStyle(el, userStyles);
+  const baseFixedStyles = position ? fixedStyles : "";
 
-	return `${baseFixedStyles}
+  return `${baseFixedStyles}
 	.share-button {
 		${fixedPositionStyles}
 	}
