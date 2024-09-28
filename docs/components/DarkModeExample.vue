59f2363a803b9ae93cdfb88dbb3e005aa63bd60a
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-09-28 14:03:20 -0400

    add dark mode component

---
+++ docs/components/DarkModeExample.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from "vue";
+onMounted(() => {
+  const share = document.querySelector("#share");
+  const toggle = document.querySelector("#toggle");
+
+  toggle.addEventListener("click", () => {
+    const state = share.getAttribute("dark-mode");
+
+    if (state === "true") {
+      share.setAttribute("dark-mode", "false");
+    } else {
+      share.setAttribute("dark-mode", "true");
+    }
+  });
+});
+</script>
+
+<template>
+  <div class="sb-container">
+    <share-button id="share"></share-button>
+    <button class="sb-button" id="toggle">Toggle dark mode</button>
+  </div>
+</template>
